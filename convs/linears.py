650bbc82a37d5011bc65715b96170e02cdf9a1b5
ADITYA DANDHARE <147025249+ADITYAD216@users.noreply.github.com>
2025-04-15 19:26:45 +0530

    Update linears.py

--- convs/linears.py
+++ convs/linears.py
@@ -1,34 +1,25 @@
-'''
-Reference:
-https://github.com/hshustc/CVPR19_Incremental_Learning/blob/master/cifar100-class-incremental/modified_linear.py
-'''
-import math
-import torch
-from torch import nn
-from torch.nn import functional as F
+class SingleLinearLayer(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SingleLinearLayer, self).__init__()
+        self.fc = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.fc(x)
 
 
-class FCSSimpleLinear(nn.Module):
-    '''
-    Reference:
-    https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/linear.py
-    '''
-    def __init__(self, in_features, out_features, bias=True):
-        super(FCSSimpleLinear, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
+class SmallMLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(SmallMLP, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, output_dim),
+        )
 
-    def reset_parameters(self):
-        nn.init.kaiming_uniform_(self.weight, nonlinearity='linear')
-        nn.init.constant_(self.bias, 0)
-
-    def forward(self, input):
-
-        return {'logits': F.linear(input, self.weight, self.bias)}
+    def forward(self, x):
+        return self.layers(x)
 
