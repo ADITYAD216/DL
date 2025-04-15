5f43df4216525c0e87340f3cca4f21c8f6e04220
ADITYA DANDHARE <147025249+ADITYAD216@users.noreply.github.com>
2025-04-15 20:00:15 +0530

    Update inc_net.py

--- utils/inc_net.py
+++ utils/inc_net.py
@@ -2,6 +2,7 @@ import copy
 import logging
 import torch
 from torch import nn
+
 from convs.cifar_resnet import resnet32
 from convs.resnet import resnet18, resnet34, resnet50
 from convs.ucir_cifar_resnet import resnet32 as cosine_resnet32
@@ -9,8 +10,8 @@ from convs.ucir_resnet import resnet18 as cosine_resnet18
 from convs.ucir_resnet import resnet34 as cosine_resnet34
 from convs.ucir_resnet import resnet50 as cosine_resnet50
 from convs.linears import SingleLinearLayer, SmallMLP
-from convs.modified_represnet import resnet18_rep,resnet34_rep
-from convs.resnet_cbam import resnet18_cbam,resnet34_cbam,resnet50_cbam
+from convs.modified_represnet import resnet18_rep, resnet34_rep
+from convs.resnet_cbam import resnet18_cbam, resnet34_cbam, resnet50_cbam
 
 
 def get_convnet(args, pretrained=False):
@@ -18,29 +19,27 @@ def get_convnet(args, pretrained=False):
     if name == "resnet32":
         return resnet32()
     elif name == "resnet18":
-        return resnet18(pretrained=pretrained,args=args)
+        return resnet18(pretrained=pretrained, args=args)
     elif name == "resnet34":
-        return resnet34(pretrained=pretrained,args=args)
+        return resnet34(pretrained=pretrained, args=args)
     elif name == "resnet50":
-        return resnet50(pretrained=pretrained,args=args)
+        return resnet50(pretrained=pretrained, args=args)
     elif name == "cosine_resnet18":
-        return cosine_resnet18(pretrained=pretrained,args=args)
+        return cosine_resnet18(pretrained=pretrained, args=args)
     elif name == "cosine_resnet32":
         return cosine_resnet32()
     elif name == "cosine_resnet34":
-        return cosine_resnet34(pretrained=pretrained,args=args)
+        return cosine_resnet34(pretrained=pretrained, args=args)
     elif name == "cosine_resnet50":
-        return cosine_resnet50(pretrained=pretrained,args=args)
+        return cosine_resnet50(pretrained=pretrained, args=args)
     elif name == "resnet18_rep":
-        return resnet18_rep(pretrained=pretrained,args=args)
+        return resnet18_rep(pretrained=pretrained, args=args)
     elif name == "resnet18_cbam":
-        return resnet18_cbam(pretrained=pretrained,args=args)
+        return resnet18_cbam(pretrained=pretrained, args=args)
     elif name == "resnet34_cbam":
-        return resnet34_cbam(pretrained=pretrained,args=args)
+        return resnet34_cbam(pretrained=pretrained, args=args)
     elif name == "resnet50_cbam":
-        return resnet50_cbam(pretrained=pretrained,args=args)
-    elif name == "resnet12":
-        return resnet12(pretrained=pretrained, args=args)
+        return resnet50_cbam(pretrained=pretrained, args=args)
     else:
         raise NotImplementedError("Unknown type {}".format(name))
 
@@ -48,7 +47,7 @@ def get_convnet(args, pretrained=False):
 class BaseNet(nn.Module):
     def __init__(self, args, pretrained):
         super(BaseNet, self).__init__()
-
+        self.args = args
         self.convnet = get_convnet(args, pretrained)
         self.fc = None
 
@@ -63,11 +62,9 @@ class BaseNet(nn.Module):
         x = self.convnet(x)
         if not isinstance(x, dict):
             x = {"features": x}  # Ensure x is a dictionary
-        out = self.fc(x["features"])
-        if isinstance(out, dict):
-            out.update(x)
-        else:
-            out = {"output": out, **x}  # Wrap output in a dictionary if needed
+        features = x["features"]
+        logits = self.fc(features)
+        out = {"logits": logits, **x}  # Use "logits" for clarity
         return out
 
     def update_fc(self, nb_classes):
@@ -88,7 +85,6 @@ class BaseNet(nn.Module):
         for param in self.parameters():
             param.requires_grad = False
         self.eval()
-
         return self
 
 
@@ -96,7 +92,7 @@ class FCSIncrementalNet(BaseNet):
     def __init__(self, args, pretrained, gradcam=False):
         super().__init__(args, pretrained)
         self.gradcam = gradcam
-        if hasattr(self, "gradcam") and self.gradcam:
+        if self.gradcam:
             self._gradcam_hooks = [None, None]
             self.set_gradcam_hook()
 
@@ -108,8 +104,6 @@ class FCSIncrementalNet(BaseNet):
             bias = copy.deepcopy(self.fc.bias.data)
             fc.weight.data[:nb_output] = weight
             fc.bias.data[:nb_output] = bias
-
-        del self.fc
         self.fc = fc
 
     def weight_align(self, increment):
@@ -123,31 +117,26 @@ class FCSIncrementalNet(BaseNet):
         self.fc.weight.data[-increment:, :] *= gamma
 
     def generate_fc(self, in_dim, out_dim):
-        fc = SingleLinearLayer(in_dim, out_dim)
-
-        return fc
+        return self.args.get("fcn_type") == "mlp" and SmallMLP(in_dim, 512, out_dim) or SingleLinearLayer(in_dim, out_dim)
 
     def forward(self, x):
         x = self.convnet(x)
         features = x["features"]
         logits = self.fc(features)
-        out = {"output": logits, **x}
+        out = {"logits": logits, **x}
 
-        if hasattr(self, "gradcam") and self.gradcam:
+        if self.gradcam:
             out["gradcam_gradients"] = self._gradcam_gradients
             out["gradcam_activations"] = self._gradcam_activations
 
         return out
 
-
- 
-
-
     def unset_gradcam_hook(self):
-        self._gradcam_hooks[0].remove()
-        self._gradcam_hooks[1].remove()
-        self._gradcam_hooks[0] = None
-        self._gradcam_hooks[1] = None
+        if self._gradcam_hooks[0]:
+            self._gradcam_hooks[0].remove()
+        if self._gradcam_hooks[1]:
+            self._gradcam_hooks[1].remove()
+        self._gradcam_hooks = [None, None]
         self._gradcam_gradients, self._gradcam_activations = [None], [None]
 
     def set_gradcam_hook(self):
@@ -155,48 +144,33 @@ class FCSIncrementalNet(BaseNet):
 
         def backward_hook(module, grad_input, grad_output):
             self._gradcam_gradients[0] = grad_output[0]
-            return None
 
         def forward_hook(module, input, output):
             self._gradcam_activations[0] = output
-            return None
 
-        self._gradcam_hooks[0] = self.convnet.last_conv.register_backward_hook(
-            backward_hook
-        )
-        self._gradcam_hooks[1] = self.convnet.last_conv.register_forward_hook(
-            forward_hook
-        )
+        self._gradcam_hooks[0] = self.convnet.last_conv.register_backward_hook(backward_hook)
+        self._gradcam_hooks[1] = self.convnet.last_conv.register_forward_hook(forward_hook)
 
 
 class FCSNet(FCSIncrementalNet):
     def __init__(self, args, pretrained, gradcam=False):
-        super().__init__(args, pretrained,gradcam)
+        super().__init__(args, pretrained, gradcam)
         self.args = args
-
         self.transfer = SingleLinearLayer(self.feature_dim, self.feature_dim)
-            
-    def update_fc(self, num_old, num_total, num_aux):
 
-        fc = self.generate_fc(self.feature_dim, num_total+num_aux)
+    def update_fc(self, num_old, num_total, num_aux):
+        fc = self.generate_fc(self.feature_dim, num_total + num_aux)
         if self.fc is not None:
             weight = copy.deepcopy(self.fc.weight.data)
             bias = copy.deepcopy(self.fc.bias.data)
             fc.weight.data[:num_old] = weight[:num_old]
             fc.bias.data[:num_old] = bias[:num_old]
-        del self.fc
         self.fc = fc
-        
 
         transfer = SingleLinearLayer(self.feature_dim, self.feature_dim)
-        
         transfer.weight = nn.Parameter(torch.eye(self.feature_dim))
         transfer.bias = nn.Parameter(torch.zeros(self.feature_dim))
-           
-        del self.transfer
         self.transfer = transfer
-
-
 
 
 class BiasLayer(nn.Module):
@@ -207,14 +181,8 @@ class BiasLayer(nn.Module):
 
     def forward(self, x, low_range, high_range):
         ret_x = x.clone()
-        ret_x[:, low_range:high_range] = (
-            self.alpha * x[:, low_range:high_range] + self.beta
-        )
+        ret_x[:, low_range:high_range] = self.alpha * x[:, low_range:high_range] + self.beta
         return ret_x
 
     def get_params(self):
         return (self.alpha.item(), self.beta.item())
-
-
-
-
